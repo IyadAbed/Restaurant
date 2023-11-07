959c83de39c06cb1e6261890993cd5e8872faa34
IyadAbed <iyadrabed@gmail.com>
2023-11-08 01:56:34 +0300

    init project

---
+++ Back/Models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
---
+++ Back/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const userRoutes = require("./Routes/userRouter");
+// const subjectRoutes = require("./Routes/subjectRoute");
+app.use(cors());
+app.use(express.json());
+app.use(userRoutes);
+// app.use(subjectRoutes);
+const port = process.env.PORT;
+const dbUrl = process.env.DBURL;
+
+module.exports = {
+  server: app,
+  start: () => {
+    mongoose
+      .connect(dbUrl)
+      .then(() => console.log("connected to mongoDB"))
+      .then(() => {
+        app.listen(port, () => {
+          console.log(`server is running on port ${port}`);
+        });
+      });
+  },
+};
